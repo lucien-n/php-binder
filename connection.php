1b427750943930db7317f930123e662ab73e65c5
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-05-30 16:39:00 +0200

    commented connection.php

--- connection.php
+++ connection.php
@@ -4,6 +4,7 @@ $user = "binder";
 $password = "binderBINDER1234*!*";
 $db = "todo";
 
+// * Initialise la connection à notre base de données
 $conn = new mysqli($host, $user, $password, $db);
 
 if ($conn->connect_error) {
