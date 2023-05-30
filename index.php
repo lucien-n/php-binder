5a08aaed06c76ef0ef816893881ff9d9c9dd878b
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-05-30 16:36:34 +0200

    commented

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
 require_once('./connection.php');
 
+// * Check si la requête est de type "submit" donc si on a cliqué sur "Submit" dans notre formulaire
 if (isset($_POST["submit"])) {
+    // * Check si le contennu n'est pas vide pour ne pas créer un todo vide
     if (empty($_POST["content"])) {
         $errors = "Empty todo";
     } else {
-        $content = $_POST["content"];
-        $sql = "INSERT INTO todos (content) VALUES ('" . $content . "')";
-        $conn->query($sql);
-        header("location: index.php");
+        // * récupère le contennu du formulaire
+        $content = $_POST["content"]; // * "content" est le tag name qu'on a set dans notre formulaire
+        $sql = "INSERT INTO todos (content) VALUES ('" . $content . "')"; // * on crée notre requête avec le contennu de la todo
+        $conn->query($sql); // * on exécute la query
+        header("location: index.php"); // * on recharge la page
     }
 }
 
+// * Check si l'argument d'url "del_todo" existe
 if (isset($_GET["del_todo"])) {
-    $todo_id = $_GET["del_todo"];
-    $conn->query("DELETE FROM todos WHERE id='" . $todo_id . "'");
-    header("location: index.php");
+    $todo_id = $_GET["del_todo"]; // * on récupère l'id donné en argument d'url "/index.php?del_todo=<id>"
+    $sql = "DELETE FROM todos WHERE id='" . $todo_id . "'"; // * on crée notre requête "DELETE" avec l'id de la todo
+    $conn->query($sql); // * on éxécute la query
+    header("location: index.php"); // * on recharge la page
 }
 ?>
 
@@ -23,11 +28,13 @@ if (isset($_GET["del_todo"])) {
 
 <body>
     <form action="/" method="POST">
+        <!-- A noter que name="content" permet de récupérer le contennu de l'input dans le php -->
         <input type="text" name="content" id="content">
         <button type="submit" name="submit">Submit</button>
     </form>
     <ul>
         <?php
+        // * Récupère tous les todos dans la base de données
         $myValues = $conn->query("SELECT * FROM todos");
 
         if ($myValues->num_rows > 0) {
