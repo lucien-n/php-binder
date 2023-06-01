82ec5ed772d14060594f9770039d0718979330ce
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-06-01 10:46:19 +0200

    wip: twig impl

--- src/auth/login.php
+++ src/auth/login.php
@@ -1,11 +1,7 @@
-<form action="/auth?login" method="POST">
-    <section>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email">
-    </section>
-    <section>
-        <label for="password">Password</label>
-        <input type="text" id="password" name="password">
-    </section>
-    <button type="submit">Login</button>
-</form>
+<?php 
+require_once($_SERVER["DOCUMENT_ROOT"] . '/twig.php');
+
+$login_template = $twig->load('login.html');
+
+echo $login_template->render();
+?>
