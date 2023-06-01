cf27a74343bf09066883c21fdfb08480f0f1f61f
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-06-01 10:03:18 +0200

    feat: db connection

--- src/connection.php
+++ src/connection.php
@@ -1,3 +1,14 @@
 <?php 
 require_once(__DIR__ . "/vendor/autoload.php");
+
+$host = $_ENV["DB_HOST"];
+$user = $_ENV["DB_USER"];
+$pass = $_ENV["DB_PASS"];
+$name = $_ENV["DB_NAME"];
+
+try {
+    $conn = new PDO('mysql:host=' . $host . ';dbname='. $name .'', $user, $pass);
+} catch (PDOException $e) {
+    die('Couldn\'t connect to ' . $host . ": " . $e->getMessage());
+}
 ?>
